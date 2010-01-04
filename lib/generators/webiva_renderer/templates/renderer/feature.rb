a060c8ad18e58f8f838e4ae6f78ae5c4ff8b235f
Doug Youch <doug@cykod.com>
2010-01-04 23:57:29 +0000

    Fixes for class name as a title. Fixed spacing

--- lib/generators/webiva_renderer/templates/renderer/feature.rb
+++ lib/generators/webiva_renderer/templates/renderer/feature.rb
@@ -1,18 +1,15 @@
 class <%= module_class %>::<%= renderer_class %>Feature < ParagraphFeature
 
-
 <%- paragraphs.each do |paragraph| -%>
   feature :<%= module_path %>_<%= renderer_path %>_<%= paragraph %>, :default_feature => <<-FEATURE
     <%= paragraph.humanize %> Feature Code...
   FEATURE
-  
 
   def <%= module_path %>_<%= renderer_path %>_<%= paragraph %>_feature(data)
     webiva_feature(:<%= module_path %>_<%= renderer_path %>_<%= paragraph %>,data) do |c|
       # c.define_tag ...
     end
   end
+
 <%- end -%>
-
-
 end
