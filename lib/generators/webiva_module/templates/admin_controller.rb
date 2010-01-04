234c2be50a086142fac2bd05c63dc4e8c7d31d5c
Doug Youch <doug@cykod.com>
2010-01-04 17:43:42 +0000

    Added options and module admin paths.  Use titleize for displaying class name as a string

--- lib/generators/webiva_module/templates/admin_controller.rb
+++ lib/generators/webiva_module/templates/admin_controller.rb
@@ -1,30 +1,32 @@
 
 class <%= class_name %>::AdminController < ModuleController
 
- component_info '<%= class_name %>', :description => '<%= class_name.humanize %> support', 
+ component_info '<%= class_name %>', :description => '<%= class_name.underscore.humanize.titleize %> support', 
                               :access => :public
                               
  # Register a handler feature
- register_permission_category :<%= name %>, "<%= class_name %>" ,"Permissions related to <%= class_name.humanize %>"
+ register_permission_category :<%= name %>, "<%= class_name %>" ,"Permissions related to <%= class_name.underscore.humanize.titleize %>"
   
- register_permissions :<%= name %>, [ [ :manage, 'Manage <%= class_name.humanize %>', 'Manage <%= class_name.humanize %>' ],
-                                  [ :config, 'Configure <%= class_name.humanize %>', 'Configure <%= class_name.humanize %>' ]
+ register_permissions :<%= name %>, [ [ :manage, 'Manage <%= class_name.underscore.humanize.titleize %>', 'Manage <%= class_name.underscore.humanize.titleize %>' ],
+                                  [ :config, 'Configure <%= class_name.underscore.humanize.titleize %>', 'Configure <%= class_name.underscore.humanize.titleize %>' ]
                                   ]
  cms_admin_paths "options",
-    "<%= class_name.humanize %> Options" => { :action => 'index' }
- 
+    "<%= class_name.underscore.humanize.titleize %> Options" => { :action => 'index' },
+    "Options" => { :controller => '/options' },
+    "Modules" => { :controller => '/modules' }
+
  permit '<%= name %>_config'
 
  public 
  
  def options
-    cms_page_path ['Options','Modules'],"<%= class_name.humanize %> Options"
+    cms_page_path ['Options','Modules'],"<%= class_name.underscore.humanize.titleize %> Options"
     
     @options = self.class.module_options(params[:options])
     
     if request.post? && @options.valid?
       Configuration.set_config_model(@options)
-      flash[:notice] = "Updated <%= class_name.humanize %> module options".t 
+      flash[:notice] = "Updated <%= class_name.underscore.humanize.titleize %> module options".t 
       redirect_to :controller => '/modules'
       return
     end    
