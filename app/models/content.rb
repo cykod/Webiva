79b63d80b121dd9bd46702b4c0a0ba5f32f50b99
Pascal Rettig <pascal@cykod.com>
2010-01-04 19:02:15 -0500

    Removed unused files

--- app/models/content.rb
+++
@@ -1,8 +0,0 @@
-# Copyright (C) 2009 Pascal Rettig.
-
-# Contains all functionality related to the custom content model system
-# with the exception of the supporting DomainModel's 
-#
-# See also: ContentModel, ContentPublication, ContentModelField, ContentPublicationField
-module Content
-end
