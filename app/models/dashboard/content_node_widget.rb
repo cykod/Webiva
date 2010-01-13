716e52e016cae25feef3cc5f4d9e1ba29a8b0692
Pascal Rettig <pascal@cykod.com>
2010-01-13 10:50:15 -0500

    Added ability to show / not show content descriptions.

--- app/models/dashboard/content_node_widget.rb
+++ app/models/dashboard/content_node_widget.rb
@@ -8,18 +8,21 @@ class Dashboard::ContentNodeWidget < Dashboard::WidgetBase
     conditions = options.content_type_ids.length > 0 ? {  :content_type_id => options.content_type_ids } : nil
     nodes = ContentNode.find(:all,:conditions => conditions, :limit => options.count, :order => "updated_at DESC" )
     
-    render_widget :partial => '/dashboard/content_node_widget', :locals => {  :content_nodes => nodes }
+    render_widget :partial => '/dashboard/content_node_widget', :locals => {  :content_nodes => nodes, :options => options }
   end
 
   class UpdatesOptions < HashModel
-    attributes :content_type_ids => [], :title => "Updated Content", :count => 10
+    attributes :content_type_ids => [], :title => "Updated Content", :count => 10, :show_description => true
     
+    boolean_options :show_description
     integer_array_options :content_type_ids
     integer_options :count
     validates_numericality_of :count
 
     options_form(
            fld(:count,:text_field,:label => "Number to show"),
+           fld(:show_description,:radio_buttons, :options => :yes_no,
+               :description => "Show description of type of content below title?"),
            fld(:content_type_ids,:ordered_array,:options => :content_type_options,
                :label => "Limit by Content Type",
                :description => "Widget will show all updated content or only specific types")
@@ -27,6 +30,9 @@ class Dashboard::ContentNodeWidget < Dashboard::WidgetBase
 
     def content_type_options
       ContentType.select_options
+    end
+    def yes_no
+      [["Yes".t,true],["No".t,false]]
     end
   end
 
