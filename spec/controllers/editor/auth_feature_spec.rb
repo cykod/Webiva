8d7aea9d75a2516b7ef1f80eb60e22025bd7d3be
Doug Youch <doug@cykod.com>
2010-01-22 15:42:17 +0000

    Test available features for AuthRenderer.

---
+++ spec/controllers/editor/auth_feature_spec.rb
@@ -0,0 +1,86 @@
+require File.dirname(__FILE__) + "/../../spec_helper"
+
+describe Editor::AuthFeature, :type => :view do
+
+  reset_domain_tables :end_users, :end_user_addresses, :tags, :end_user_tags, :site_nodes, :user_subscriptions, :mail_templates, :user_subscription_entries
+
+  before(:each) do
+    @feature = build_feature('/editor/auth_feature')
+  end
+
+  it "should display user_register feature" do
+    @success_page = SiteNode.create :title => 'success', :site_version_id => 1
+    @options = Editor::AuthController::UserRegisterOptions.new :success_page_id => @success_page.id, :user_class_id => UserClass.default_user_class_id
+    @usr = EndUser.new :email => 'test@test.dev'
+    @address = @usr.build_address(:address_name => 'Default Address'.t )
+    @business = @usr.build_address(:address_name => 'Business Address'.t )
+
+    data = { :options => @options,
+             :usr => @usr,
+             :address => @address,
+             :business => @business
+           }
+  
+    @output = @feature.user_register_feature(data)
+    @output.should include('test@test.dev')
+  end
+
+  it "should display user_activation feature" do
+    opts = {}
+    @options = Editor::AuthController::UserActivationOptions.new opts
+    @user = EndUser.new
+
+    data = { :options => @options,
+             :user => @user,
+             :status => 'activation',
+             :activation_object => DefaultsHashObject.new(:code => 'testcode', :accept => false )
+           }
+  
+    @output = @feature.user_activation_feature(data)
+    @output.should include('testcode')
+  end
+
+  it "should display login feature" do
+    opts = {}
+    @options = Editor::AuthController::LoginOptions.new opts
+    @login_user = EndUser.new
+
+    data = { :login_user => @login_user,
+             :type => @options.login_type
+           }
+  
+    @output = @feature.login_feature(data)
+    @output.should include('cms_login[login]')
+  end
+
+  it "should display missing_password feature" do
+    data = { :invalid => nil,
+             :state => 'missing_password'
+           }
+  
+    @feature.should_receive(:url_for).and_return('')
+    @output = @feature.missing_password_feature(data)
+    @output.should include('missing_password[email]')
+  end
+
+  it "should display enter_vip feature" do
+    data = { :failure => false,
+             :registered => false }
+  
+    @output = @feature.enter_vip_feature(data)
+    @output.should include('vip[number]')
+  end
+
+  it "should display email_list feature" do
+    @subscription = UserSubscription.create :name => 'Test'
+    @destination_page = SiteNode.create(:title => 'destiny', :site_version_id => 1)
+    @options = Editor::AuthController::UserActivationOptions.new :user_subscription_id => @subscription.id, :destination_page_id => @destination_page.id
+
+    data = { :email_list => Editor::AuthRenderer::EmailListUser.new(:email => 'test@test.dev'),
+             :options => @options,
+             :submitted => nil }
+  
+    @output = @feature.email_list_feature(data)
+    @output.should include('test@test.dev')
+  end
+end
