e6ecdf6349e263e4c5825167418b9612547b13a5
Pascal Rettig <pascal@cykod.com>
2010-01-27 14:21:48 -0500

    Added automatic conversion to UTC for AR parameters (Damn you rails, you should do this automagically!)

--- lib/webiva_monkey_patches.rb
+++ lib/webiva_monkey_patches.rb
@@ -144,6 +144,21 @@ class Date  #:nodoc:all
         end
 end 
 
+
+module ActiveRecord
+  module ConnectionAdapters # :nodoc:
+    module Quoting
+      # Convert dates and times to UTC so that the following two will be equivalent:
+      # Event.all(:conditions => ["start_time > ?", Time.zone.now])
+      # Event.all(:conditions => ["start_time > ?", Time.now])
+      def quoted_date(value)
+        value.respond_to?(:utc) ? value.utc.to_s(:db) : value.to_s(:db)
+      end
+    end
+  end
+end
+
+
 #class CGI::Session::MemCacheStore
 # def check_id(id) #:nodoc:# 
 #     /[^0-9a-zA-Z\-._]+/ =~ id.to_s ? false : true 
