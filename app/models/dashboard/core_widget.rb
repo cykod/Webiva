02cc9661df969628cee081cd6083ef462c510c93
Doug Youch <doug@cykod.com>
2010-01-27 22:53:44 +0000

    Added RSS Feed Viewer widget.

--- app/models/dashboard/core_widget.rb
+++ app/models/dashboard/core_widget.rb
@@ -1,10 +1,12 @@
 
+require 'rss/2.0'
 
 class Dashboard::CoreWidget < Dashboard::WidgetBase #:nodoc:all
 
   widget :information, :name => "Display Static Information", :title => "Site Info"
   widget :emarketing_stats, :name => "Emarketing: Display Real Time Page Views", :title => "Real Time Page Views"
   widget :emarketing_charts, :name => "Emarketing: Uniques / Page Views Chart", :title => "Uniques / Page Views"
+  widget :rss_viewer, :name => "Display RSS Feeds", :title => 'RSS Feed'
 
   def information
     set_icon 'news_icon.png'
@@ -56,5 +58,56 @@ class Dashboard::CoreWidget < Dashboard::WidgetBase #:nodoc:all
   end
 
   class EmarketingChartsOptions < HashModel
+  end
+
+  def rss_viewer
+
+    rss_items = editor_widget.cache_fetch('Widget')
+
+    if ! rss_items || rss_items.length == 0
+      begin
+	uri = URI.parse(@options.url)
+	raise "Invalid URL" unless uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+
+	rss_feed = ''
+	timeout(@options.timeout_in_seconds) do
+	  rss_feed = RSS::Parser.parse(Net::HTTP.get(uri),false)
+	end
+
+	rss_items = []
+	rss_feed.items[0..@options.show_first-1].each { |item| rss_items << {'link' => item.link, 'title' => item.title, 'date' => item.pubDate} } if rss_feed.is_a?(RSS::Rss)
+      rescue TimeoutError
+	editor_widget.cache_put('Widget', [], nil, @options.valid_for.minutes)
+	logger.warn( "timedout fetching rss feed for #{@options.url}" )
+	render_widget :text => 'Timedout fetching RSS feed.'
+	return
+      rescue Exception => e
+	editor_widget.cache_put('Widget', [], nil, @options.valid_for.minutes)
+	logger.warn( "failed to fetch rss feed for #{@options.url}, errror: #{e}" )
+	render_widget :text => 'Failed to fetch RSS feed.'
+	return
+      end
+
+      editor_widget.cache_put('Widget', rss_items, nil, @options.valid_for.minutes)
+    end
+
+    render_widget :partial => '/dashboard/rss_viewer', :locals => {:rss_items => rss_items, :url => @options.url}
+  end
+
+  class RssViewerOptions < HashModel
+    attributes :url => nil, :show_first => 20, :timeout_in_seconds => 3, :valid_for => 5
+
+    validates_presence_of :url, :show_first, :timeout_in_seconds, :valid_for
+    validates_numericality_of :timeout_in_seconds, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 30
+    validates_numericality_of :valid_for, :greater_than_or_equal_to => 2
+
+    integer_options :show_first, :timeout_in_seconds, :valid_for
+
+    options_form(
+         fld(:url, :text_field, :label => 'RSS Url'),
+         fld(:show_first, :text_field, :description => 'items'),
+         fld(:timeout_in_seconds, :text_field, :label => 'Fetch timeout', :description => 'seconds'),
+         fld(:valid_for, :text_field, :label => 'Fetch feed every', :description => 'minutes')
+         )
   end
 end
