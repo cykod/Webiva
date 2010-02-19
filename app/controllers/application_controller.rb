b0792bc01690e9f5ae187d102765ed4bd259532f
Pascal Rettig <pascal@cykod.com>
2010-02-19 13:50:48 -0500

    Removed session clearing and added publication to default default feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -250,7 +250,7 @@ class ApplicationController < ActionController::Base
   # Convenience method to log a user in 
   # sets the session and remember cookie 
   def process_login(usr,remember = false)
-    session.clear
+#    session.clear
     session[:user_id] = usr.id
     session[:user_model] = usr.class.to_s
     reset_myself
