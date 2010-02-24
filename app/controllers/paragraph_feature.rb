a2b49fe7ffe2de6296c7eda62bb6b7aa44a78aa9
Pascal Rettig <pascal@cykod.com>
2010-02-24 14:49:39 -0500

    Fixed tag used for form

--- app/controllers/paragraph_feature.rb
+++ app/controllers/paragraph_feature.rb
@@ -743,7 +743,7 @@ block is non-nil
           frm_opts = opts.delete(:html) || { }
           frm_opts[:method] = 'post'
           html_options = html_options_for_form(options.delete(:url),frm_opts)
-          frm_tag = tag(:form,html_options) + "<CMS:AUTHENTICITY_TOKEN/>" + opts.delete(:code).to_s
+          frm_tag = tag(:form,html_options,true) + "<CMS:AUTHENTICITY_TOKEN/>" + opts.delete(:code).to_s
           cms_unstyled_fields_for(arg,obj,opts) do |f|
             tag.locals.send("#{frm_obj}=",f)
             frm_tag + tag.expand + "</form>"
