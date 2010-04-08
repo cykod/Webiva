d7d9d531f7e81b8b81388daf78447535eaf112fe
Pascal Rettig <pascal@cykod.com>
2010-04-08 09:42:17 -0400

    Added renderer_builder method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -62,6 +62,25 @@ def reset_domain_tables(*tables)
   before_each_parts << callback
 end
 
+def renderer_builder(name,&block)
+  parts = name.split("/")
+
+  define_method("#{parts[-1]}_renderer_helper") do |opts,conns|
+    if block
+      opt_opts = yield
+      opts = opt_opts.merge(opts)
+    end
+    build_renderer("/#{parts[1]}",name,opts,conns)
+  end
+  
+  class_eval <<-METHOD
+   def #{parts[-1]}_renderer(opts={},conns={})
+      #{parts[-1]}_renderer_helper(opts,conns)
+   end
+  METHOD
+
+
+end
 
 def add_factory_girl_path(dir)
   if !Factory.definition_file_paths.include?(dir)
