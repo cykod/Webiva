43a98fd58cfb38610615c53d6c0c2afc5d7dd657
Doug Youch <doug@cykod.com>
2010-05-13 23:52:43 +0000

    Fixes for user segment types. Added specs for most basic parts of user segments.

---
+++ spec/models/user_segment/field_spec.rb
@@ -0,0 +1,62 @@
+require File.dirname(__FILE__) + "/../../spec_helper"
+
+describe UserSegment::Field do
+
+  reset_domain_tables :end_users
+
+  @handler = EndUserSegmentField.user_segment_fields_handler_info
+
+  before(:each) do
+    EndUser.push_target('test1@test.dev', :created_at => 2.days.ago, :activated => true)
+    EndUser.push_target('test2@test.dev', :created_at => 5.days.ago, :activated => false)
+    EndUser.push_target('test3@test.dev', :activated => false)
+  end
+
+  it "should be able to use handler" do
+    @field = UserSegment::Field.new :field => 'created', :operation => 'before', :arguments => [1, 'days']
+    @field.handler = @handler
+
+    @field.handler_class.should == EndUserSegmentField
+    @field.domain_model_class.should == EndUser
+    @field.end_user_field.should == :id
+    @field.model_field.should == :created_at
+    @field.type_class.should == UserSegment::CoreType::DateTimeType
+    @field.operation_arguments.should == [:integer, :option]
+    @field.valid_arguments?.should == true
+    @field.valid?.should == true
+  end
+
+  it "should return the count" do
+    @field = UserSegment::Field.new :field => 'created', :operation => 'before', :arguments => [1, 'days']
+    @field.handler = @handler
+    @field.valid?.should == true
+    @field.count.should == 2
+    @field.end_user_ids.length.should == 2
+
+    @field = UserSegment::Field.new :field => 'created', :operation => 'since', :arguments => [1, 'days']
+    @field.handler = @handler
+    @field.valid?.should == true
+    @field.count.should == 1
+    @field.end_user_ids.length.should == 1
+  end
+
+  it "should work with children" do
+    @field = UserSegment::Field.new :field => 'created', :operation => 'before', :arguments => [1, 'days'], :child => {:field => 'activated', :operation => 'is', :arguments => [true]}
+    @field.handler = @handler
+    @field.valid?.should == true
+    @field.count.should == 1
+    @field.end_user_ids.length.should == 1
+
+    @field = UserSegment::Field.new :field => 'created', :operation => 'before', :arguments => [1, 'days'], :child => {:field => 'activated', :operation => 'is', :arguments => [true], :child => {:field => 'email', :operation => 'like', :arguments => ['test%@test.dev']}}
+    @field.handler = @handler
+    @field.valid?.should == true
+    @field.count.should == 1
+    @field.end_user_ids.length.should == 1
+
+    @field = UserSegment::Field.new :field => 'created', :operation => 'before', :arguments => [1, 'days'], :child => {:field => 'activated', :operation => 'is', :arguments => [false], :child => {:field => 'email', :operation => 'like', :arguments => ['test%@test.dev']}}
+    @field.handler = @handler
+    @field.valid?.should == true
+    @field.count.should == 1
+    @field.end_user_ids.length.should == 1
+  end
+end
---
+++ spec/models/user_segment/field_type_spec.rb
@@ -0,0 +1,90 @@
+require File.dirname(__FILE__) + "/../../spec_helper"
+
+describe UserSegment::FieldType do
+
+  it "should convert value to integer" do
+    UserSegment::FieldType.convert_to("0", :integer).should == 0
+    UserSegment::FieldType.convert_to(0, :integer).should == 0
+    UserSegment::FieldType.convert_to("1", :integer).should == 1
+    UserSegment::FieldType.convert_to(99, :integer).should == 99
+    UserSegment::FieldType.convert_to(99.0, :integer).should be_nil
+    UserSegment::FieldType.convert_to("99", :integer).should == 99
+    UserSegment::FieldType.convert_to("1n", :integer).should be_nil
+  end
+
+  it "should convert value to float" do
+    UserSegment::FieldType.convert_to("0", :float).should == 0.0
+    UserSegment::FieldType.convert_to(0, :float).should == 0.0
+    UserSegment::FieldType.convert_to("1", :float).should == 1.0
+    UserSegment::FieldType.convert_to(99, :float).should == 99.0
+    UserSegment::FieldType.convert_to(99.0, :float).should == 99.0
+    UserSegment::FieldType.convert_to("99", :float).should == 99.0
+    UserSegment::FieldType.convert_to("3.54", :float).should == 3.54
+    UserSegment::FieldType.convert_to(3.54, :float).should == 3.54
+    UserSegment::FieldType.convert_to("1n", :float).should be_nil
+  end
+
+  it "should only return string values" do
+    UserSegment::FieldType.convert_to("hello world", :string).should == "hello world"
+    UserSegment::FieldType.convert_to(1, :string).should be_nil
+    UserSegment::FieldType.convert_to(true, :string).should be_nil
+    UserSegment::FieldType.convert_to(false, :string).should be_nil
+    UserSegment::FieldType.convert_to(nil, :string).should be_nil
+    UserSegment::FieldType.convert_to(0.1, :string).should be_nil
+    UserSegment::FieldType.convert_to(Time.now, :string).should be_nil
+  end
+
+  it "should convert value to time" do
+    time = Time.parse "1/1/1990"
+    UserSegment::FieldType.convert_to(time.strftime("1/1/1990"), :datetime).should == time
+    time = Time.now
+    UserSegment::FieldType.convert_to(time, :datetime).should == time
+  end
+
+  it "should convert value to boolean" do
+    UserSegment::FieldType.convert_to(true, :boolean).should == true
+    UserSegment::FieldType.convert_to('true', :boolean).should == true
+    UserSegment::FieldType.convert_to('TRUE', :boolean).should == true
+    UserSegment::FieldType.convert_to(1, :boolean).should == true
+    UserSegment::FieldType.convert_to('1', :boolean).should == true
+    UserSegment::FieldType.convert_to(false, :boolean).should == false
+    UserSegment::FieldType.convert_to('false', :boolean).should == false
+    UserSegment::FieldType.convert_to('FALSE', :boolean).should == false
+    UserSegment::FieldType.convert_to(0, :boolean).should == false
+    UserSegment::FieldType.convert_to('0', :boolean).should == false
+  end
+
+  it "should convert value to option" do
+    UserSegment::FieldType.convert_to('day', :option, :options => ['day', 'days', 'week', 'weeks']).should == 'day'
+    UserSegment::FieldType.convert_to('Day', :option, :options => ['day', 'days', 'week', 'weeks']).should == 'day'
+    UserSegment::FieldType.convert_to('DAY', :option, :options => ['day', 'days', 'week', 'weeks']).should == 'day'
+    UserSegment::FieldType.convert_to('weeks', :option, :options => ['day', 'days', 'week', 'weeks']).should == 'weeks'
+    UserSegment::FieldType.convert_to('WeeKs', :option, :options => ['day', 'days', 'week', 'weeks']).should == 'weeks'
+    UserSegment::FieldType.convert_to('WeEk', :option, :options => ['day', 'days', 'week', 'weeks']).should == 'week'
+    UserSegment::FieldType.convert_to('now', :option, :options => ['day', 'days', 'week', 'weeks']).should be_nil
+    UserSegment::FieldType.convert_to('dayss', :option, :options => ['day', 'days', 'week', 'weeks']).should be_nil
+  end
+
+  it "should covert all the arguments" do
+    arguments = ['1', 'test', 'true', '1/1/1990', 'Days', '1day']
+    types = [:integer, :string, :boolean, :datetime, :option, :integer]
+    options = [{}, {}, {}, {}, {:options => ['day', 'days', 'week', 'weeks']}]
+
+    converted = UserSegment::FieldType.convert_arguments(arguments, types, options)
+    converted[0].should == 1
+    converted[1].should == 'test'
+    converted[2].should == true
+    converted[3].should == Time.parse('1/1/1990')
+    converted[4].should == 'days'
+    converted[5].should be_nil
+  end
+
+  it "should be able to register and operation" do
+    UserSegment::FieldType.register_operation(:from, [['Value', :integer], ['Option', :option, {:options => ['day', 'days']}]])
+    UserSegment::FieldType.has_operation?('from').should be_true
+    operation = UserSegment::FieldType.user_segment_field_type_operations[:from]
+    converted = UserSegment::FieldType.convert_arguments([1, 'Day'], operation[:arguments], operation[:argument_options])
+    converted[0].should == 1
+    converted[1].should == 'day'
+  end
+end
