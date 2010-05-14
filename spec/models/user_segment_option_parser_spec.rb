0d808dd1b9b399112fa1321a043b56c0ef119b3e
Doug Youch <doug@cykod.com>
2010-05-14 16:26:49 +0000

    Fix for strings.  Test the parser

---
+++ spec/models/user_segment_option_parser_spec.rb
@@ -0,0 +1,92 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+require 'treetop'
+
+describe UserSegmentOptionParser do
+
+  def parse(text)
+    parser = UserSegmentOptionParser.new
+    if data = parser.parse(text)
+      data.eval
+    else
+      raise parser.inspect
+      nil
+    end
+  end
+
+  it "should be able to parse operations to and array" do
+    code = <<-CODE
+    created.since(1, "days")
+    CODE
+    parse(code).should == [[nil, {:field => 'created', :operation => 'since', :arguments => [1, "days"], :child => nil}]]
+
+    code = <<-CODE
+    created_at.since_ago(1, "days")
+    CODE
+    parse(code).should == [[nil, {:field => 'created_at', :operation => 'since_ago', :arguments => [1, "days"], :child => nil}]]
+
+    code = <<-CODE
+    created_at2.since_ago8(1, "days")
+    CODE
+    parse(code).should == [[nil, {:field => 'created_at2', :operation => 'since_ago8', :arguments => [1, "days"], :child => nil}]]
+
+    code = <<-CODE
+    test.contains("")
+    CODE
+    parse(code).should == [[nil, {:field => 'test', :operation => 'contains', :arguments => [''], :child => nil}]]
+
+    code = <<-CODE
+    test.contains(true, false)
+    CODE
+    parse(code).should == [[nil, {:field => 'test', :operation => 'contains', :arguments => [true, false], :child => nil}]]
+
+    code = 'test.contains("days\"", 1)'
+    parse(code).should == [[nil, {:field => 'test', :operation => 'contains', :arguments => ['days"', 1], :child => nil}]]
+
+    code = <<-CODE
+    test.contains('1.1')
+    CODE
+    parse(code).should == [[nil, {:field => 'test', :operation => 'contains', :arguments => ['1.1'], :child => nil}]]
+
+    code = "test.contains('1.1\\'')"
+    parse(code).should == [[nil, {:field => 'test', :operation => 'contains', :arguments => ["1.1'"], :child => nil}]]
+
+    code = <<-CODE
+    test.contains(1.1)
+    CODE
+    parse(code).should == [[nil, {:field => 'test', :operation => 'contains', :arguments => [1.1], :child => nil}]]
+
+    code = <<-CODE
+    test.contains(1.0)
+    CODE
+    parse(code).should == [[nil, {:field => 'test', :operation => 'contains', :arguments => [1.0], :child => nil}]]
+
+    code = <<-CODE
+    test.contains(.1)
+    CODE
+    parse(code).should == [[nil, {:field => 'test', :operation => 'contains', :arguments => [0.1], :child => nil}]]
+  end
+
+  it "should be able to not an operation" do
+    code = <<-CODE
+    Not created.since(1, "days")
+    CODE
+    parse(code).should == [['not', {:field => 'created', :operation => 'since', :arguments => [1, "days"], :child => nil}]]
+  end
+
+  it "should be able to chain operations" do
+    code = <<-CODE
+    created.since(1, "days").registered.is(true)
+    CODE
+    parse(code).should == [[nil, {:field => 'created', :operation => 'since', :arguments => [1, "days"], :child => {:field => 'registered', :operation => 'is', :arguments => [true], :child => nil}}]]
+
+    code = <<-CODE
+    created.since(1, "days").registered.is(true).logged_in.is(false)
+    CODE
+    parse(code).should == [[nil, {:field => 'created', :operation => 'since', :arguments => [1, "days"], :child => {:field => 'registered', :operation => 'is', :arguments => [true], :child => {:field => 'logged_in', :operation => 'is', :arguments => [false], :child => nil}}}]]
+
+    code = <<-CODE
+    NOT created.since(1, "days").registered.is(true).logged_in.is(false)
+    CODE
+    parse(code).should == [['not', {:field => 'created', :operation => 'since', :arguments => [1, "days"], :child => {:field => 'registered', :operation => 'is', :arguments => [true], :child => {:field => 'logged_in', :operation => 'is', :arguments => [false], :child => nil}}}]]
+  end
+end
