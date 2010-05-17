8558a233397d555daea8f22680128fe5a3fd1327
Doug Youch <doug@cykod.com>
2010-05-17 14:31:56 +0000

    Don't store user objects in user segment cache.  Batch fetch users.

--- app/models/user_segment_cache.rb
+++ app/models/user_segment_cache.rb
@@ -1,5 +1,8 @@
 
 class UserSegmentCache < DomainModel
+  SIZE = 25000
+  DEFAULT_BATCH_SIZE = 1000
+
   belongs_to :user_segment
 
   serialize :id_list
@@ -10,22 +13,44 @@ class UserSegmentCache < DomainModel
     self.created_at = Time.now unless self.created_at
   end
 
-  def end_users(opts={})
-    return @end_users if @end_users
-    users_by_id = EndUser.find(:all, :conditions => {:id => self.id_list}).index_by(&:id)
-    @end_users = self.id_list.map { |id| users_by_id[id] }.compact
+  def fetch_users(opts={})
+    ids = opts[:offset] && opts[:limit] ? self.id_list[opts[:offset]..opts[:offset]+opts[:limit]-1] : self.id_list
+    users_by_id = EndUser.find(:all, :conditions => {:id => ids}).index_by(&:id)
+    ids.map { |id| users_by_id[id] }.compact
+  end
+
+  def find_in_batches(opts={})
+    limit = opts[:batch_size] || DEFAULT_BATCH_SIZE
+    offset = 0
+    num_chunks = (self.id_list.size / limit).to_i
+    num_chunks = num_chunks.succ if (self.id_list.length % limit) > 0
+    (1..num_chunks).each do |chunk|
+      users = self.fetch_users(:offset => offset, :limit => limit)
+      offset = offset + limit
+      yield users
+    end
   end
 
   def each(opts={})
-    self.end_users(opts).each { |user| yield user }
+    self.find_in_batches(opts) { |users| users.each { |user| yield user } }
+  end
+
+  def collect(opts={})
+    data = []
+    self.find_in_batches(opts) { |users| data = data + users.collect { |user| yield user } }
+    data
   end
 
   def each_with_index(idx=0, opts={})
-    self.end_users(opts).each { |user| yield user, idx; idx = idx.succ }
+    self.find_in_batches(opts) { |users| users.each { |user| yield user, idx; idx = idx.succ } }
     idx
   end
 
   def find(opts={})
-    self.end_users(opts).find { |user| yield user }
+    self.find_in_batches(opts) do |users|
+      user = users.find { |user| yield user }
+      return user if user
+    end
+    nil
   end
 end
