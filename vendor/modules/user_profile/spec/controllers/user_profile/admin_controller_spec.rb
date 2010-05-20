615c6baa7ce81ad3f485269baae024f33800b851
Pascal Rettig <pascal@cykod.com>
2010-05-20 16:52:34 -0400

    Fixed order of resets to solve testing issue

--- vendor/modules/user_profile/spec/controllers/user_profile/admin_controller_spec.rb
+++ vendor/modules/user_profile/spec/controllers/user_profile/admin_controller_spec.rb
@@ -4,11 +4,11 @@ describe UserProfile::AdminController do
   reset_domain_tables :user_profile_types, :configurations, :site_modules, :end_users, :end_user_addresses, :user_profile_entries
 
   before(:each) do
+    mock_editor
     test_activate_module('user_profile')
   end
 
   it 'should create default profile type on mod set up' do
-    mock_editor
     assert_difference "UserProfileType.count", 1 do
       get "options"
     end
