b2d4e25d7684616472989347de0efa5b27fd6a77
Doug Youch <doug@cykod.com>
2010-06-07 20:38:52 +0000

    Adding tests for sortable fields and field output.

--- app/models/end_user_action_segment_field.rb
+++ app/models/end_user_action_segment_field.rb
@@ -8,6 +8,7 @@ class EndUserActionSegmentField < UserSegment::FieldHandler
     }
   end
 
+  register_field :num_actions, UserSegment::CoreType::CountType, :field => :end_user_id, :name => '# Actions', :display_method => 'count', :sort_method => 'count', :sortable => true
   register_field :user_action, EndUserActionSegmentType::UserActionType, :field => [:renderer, :action], :name => 'User Actions', :display_field => :description, :sortable => true, :display_field => :description
   register_field :action, EndUserActionSegmentType::ActionType, :name => 'User Actions: Action', :sortable => true
   register_field :renderer, EndUserActionSegmentType::RendererType, :name => 'User Actions: Renderer', :sortable => true
@@ -15,8 +16,14 @@ class EndUserActionSegmentField < UserSegment::FieldHandler
   register_field :occurred, UserSegment::CoreType::DateTimeType, :field => :action_at, :name => 'User Actions: Occurred', :sortable => true
 
   def self.sort_scope(order_by, direction)
+     info = UserSegment::FieldHandler.sortable_fields[order_by.to_sym]
+
     if order_by.to_sym == :user_action
       EndUserAction.scoped :order => "renderer #{direction}, action #{direction}"
+    elsif order_by.to_sym == :num_actions
+      sort_method = info[:sort_method]
+      field = info[:field]
+      EndUserAction.scoped(:select => "end_user_id, #{sort_method}(#{field}) as #{field}_#{sort_method}", :group => :end_user_id, :order => "#{field}_#{sort_method} #{direction}")
     else
       field = self.user_segment_fields[order_by.to_sym][:field]
       EndUserAction.scoped :order => "#{field} #{direction}"
