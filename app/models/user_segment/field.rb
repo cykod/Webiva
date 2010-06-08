1d0cf596855a485c0e647e931091c1c5a5fa1ab5
Doug Youch <doug@cykod.com>
2010-06-08 18:33:32 +0000

    Fix, defautl scoped must be scoped around any existings scope. Important when combining scopes.

--- app/models/user_segment/field.rb
+++ app/models/user_segment/field.rb
@@ -78,24 +78,16 @@ class UserSegment::Field < HashModel
     scope.find(:all).collect &self.end_user_field
   end
 
-  def get_default_scope
-    scope = self.handler_class.user_segment_fields[self.field.to_sym][:scope]
-    if scope
-      if scope.is_a?(Hash)
-        self.domain_model_class.scoped scope
-      elsif scope.is_a?(Proc)
-        scope.call
-      elsif scope.is_a?(ActiveRecord::NamedScope::Scope)
-        scope
-      end
-    else
-      self.domain_model_class
-    end
+  def get_default_scope(scope=nil)
+    scope ||= self.domain_model_class
+    base_scope = self.handler_class.user_segment_fields[self.field.to_sym][:scope]
+    scope.scoped base_scope if base_scope
+    scope
   end
 
   def get_scope(scope=nil)
     return @scope if @scope
-    @scope = self.type_class.send(self.operation, self.get_default_scope, self.end_user_field, self.model_field, *self.converted_arguments)
+    @scope = self.type_class.send(self.operation, self.get_default_scope(scope), self.end_user_field, self.model_field, *self.converted_arguments)
     @scope = self.child_field.get_scope(@scope) if self.child_field
     @scope
   end
