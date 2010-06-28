c36e9150142d2385dedb80c2f60df9af7b898102
Pascal Rettig <pascal@cykod.com>
2010-06-28 10:28:27 -0400

    Fix for invalid modifier_data

--- vendor/modules/feed/app/controllers/feed/data_output_dispatcher.rb
+++ vendor/modules/feed/app/controllers/feed/data_output_dispatcher.rb
@@ -8,7 +8,7 @@ class Feed::DataOutputDispatcher < ModuleDispatcher
                   
  
   def data_output(args)
-
+    @site_node.page_modifier.modifier_data ||= {}
   
     simple_dispatch(1,'editor/publication','data_output',:data => @site_node.page_modifier.modifier_data, :content_publication_id => @site_node.page_modifier.modifier_data[:data_publication_id].to_i, :site_feature_id => @site_node.page_modifier.modifier_data[:site_feature_id].to_i )
   end
