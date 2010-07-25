3d0b332f369972479e9974304205412a2c1aa0f0
Pascal Rettig <pascal@cykod.com>
2010-07-25 12:23:47 -0400

    Separated starling config to defaults.yml file

--- script/starling.rb
+++ script/starling.rb
@@ -4,14 +4,20 @@ require 'yaml'
 
 path = File.dirname(__FILE__)
 
-config = YAML.load_file(path + "/../config/workling.yml")
+config = YAML.load_file(path + "/../config/defaults.yml")
 
 env = ENV['RAILS_ENV'] || 'production'
 
-config = config[env] 
+if !config['starling']
+ if env == 'production'
+  config['starling'] = 'localhost:15151'
+ else
+  config['starling'] = 'localhost:22122'
+ end
+end 
 
-host = config['listens_on'].split(":")[0]
-port = config['listens_on'].split(":")[1]
+host = config['starling'].split(":")[0]
+port = config['starling'].split(":")[1]
 
 cmd = "starling -d -P #{path}/../tmp/pids/starling.pid -q  #{path}/../tmp/starling_#{env}/ -h #{host} -p #{port}"
 puts("Running #{env}:" + cmd)
