c8d892fa03e5dedb65f75481c7be8dbd67c6e513
Pascal Rettig <pascal@cykod.com>
2010-10-20 17:43:24 -0400

    Fix missing prefix problem

--- app/controllers/editor/content_renderer.rb
+++ app/controllers/editor/content_renderer.rb
@@ -9,6 +9,7 @@ class Editor::ContentRenderer < ParagraphRenderer #:nodoc:all
     @options = paragraph_options(:recent_content)
 
     conditions = @options.content_type_ids.length > 0 ? {  :content_type_id => @options.content_type_ids } : nil
+    conditions[:published] = true
     @nodes = ContentNode.find(:all,:conditions => conditions, :limit => @options.count, :order => @options.order )
 
     render_paragraph :feature => :recent_content
