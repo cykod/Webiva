e4d8ad38a796943dc4dc4b8cda3f920cd4044c88
Pascal Rettig <pascal@cykod.com>
2010-12-01 15:42:57 -0500

    Added help back in to titlebar

--- vendor/modules/webiva_net/app/models/webiva_net/titlebar_handler.rb
+++ vendor/modules/webiva_net/app/models/webiva_net/titlebar_handler.rb
@@ -14,6 +14,6 @@ class WebivaNet::TitlebarHandler
     opts = WebivaNet::AdminController.module_options
     params = @ctrl.send(:params)
     ref_string = params[:controller] + "/" + params[:action]
-    "<a target='_blank' href='#{opts.documentation_url}?ref=#{ref_string}'><img src='#{@ctrl.send(:theme_src,"framework/page_title_help_icon.gif")}' align='absmiddle' title='#{@ctrl.send(:vh,"Webiva.net Help on this Page".t)}' /></a>"
+    "<ul class='action_panel'><li class='right'><a target='_blank' title='#{"Help about this page".t}' href='#{opts.documentation_url}?ref=#{ref_string}'>?</a></li></ul>"
   end
 end
