cc29b519c49822f9b04b5bb1f50ff20fab49cb3d
Pascal Rettig <pascal@cykod.com>
2010-12-07 15:00:27 -0500

    Added introductory screen

--- vendor/modules/webiva_net/app/models/webiva_net/titlebar_handler.rb
+++ vendor/modules/webiva_net/app/models/webiva_net/titlebar_handler.rb
@@ -14,6 +14,15 @@ class WebivaNet::TitlebarHandler
     opts = WebivaNet::AdminController.module_options
     params = @ctrl.send(:params)
     ref_string = params[:controller] + "/" + params[:action]
-    "<ul class='action_panel'><li class='right'><a target='_blank' title='#{"Help about this page".t}' href='#{opts.documentation_url}?ref=#{ref_string}'>?</a></li></ul>"
+    titlebar_str = "<ul class='action_panel'><li class='right'><a target='_blank' title='#{"Help about this page".t}' href='#{opts.documentation_url}?ref=#{ref_string}'>?</a></li></ul>"
+    if(params[:controller] == 'dashboard' && params[:action] == 'index') 
+      if SiteNode.count < 3
+        titlebar_str << "\n<script>
+         $j(function() { SCMS.remoteOverlay('/website/webiva_net/themes/welcome'); });
+       </script>"
+      end
+    end
+
+    titlebar_str
   end
 end
