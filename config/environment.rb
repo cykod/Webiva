8b19a4e41aff49c0e0e7ee5932d0b7e045986058
Doug Youch <doug@cykod.com>
2010-12-08 18:19:26 -0500

    Create a symlink to a modules public folder

--- config/environment.rb
+++ config/environment.rb
@@ -152,11 +152,10 @@ ActionMailer::Base.logger = nil unless Rails.env == 'development'
 # Copy Assets over
 
 Dir.glob("#{RAILS_ROOT}/vendor/modules/[a-z]*") do |file|
-  if file =~ /\/([a-z_-]+)\/{0,1}$/
+  if file =~ /\/([a-z0-9_-]+)\/{0,1}$/
     mod_name = $1
-    if File.directory?(file + "/public")
-      FileUtils.mkpath("#{RAILS_ROOT}/public/components/#{mod_name}")
-      FileUtils.cp_r(Dir.glob(file + "/public/*"),"#{RAILS_ROOT}/public/components/#{mod_name}/")
+    if File.directory?(file + "/public") && ! File.exists?("#{RAILS_ROOT}/public/components/#{mod_name}")
+      FileUtils.symlink(file + "/public", "#{RAILS_ROOT}/public/components/#{mod_name}")
     end
   end
 end
