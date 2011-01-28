cdfbc24c3dc6c1c01b42c86045eda67d57e563b2
Clinton Judy and Scott Woods <developers+clinton+scott@westarete.com>
2011-01-28 00:02:06 -0500

    Fix bug where we were testing for empty but not nil.
    Use blank?, not empty?.

--- vendor/modules/next_steps/app/controllers/next_steps/page_renderer.rb
+++ vendor/modules/next_steps/app/controllers/next_steps/page_renderer.rb
@@ -8,9 +8,9 @@ class NextSteps::PageRenderer < ParagraphRenderer
     @options = paragraph_options(:view)
     @step_options = NextStepsStep.all.collect{|s| [s.to_s, s.id] }
     @steps = []
-    @steps << NextStepsStep.find(@options.step_1) unless @options.step_1.empty? 
-    @steps << NextStepsStep.find(@options.step_2) unless @options.step_2.empty?
-    @steps << NextStepsStep.find(@options.step_3) unless @options.step_3.empty?
+    @steps << NextStepsStep.find(@options.step_1) unless @options.step_1.blank? 
+    @steps << NextStepsStep.find(@options.step_2) unless @options.step_2.blank?
+    @steps << NextStepsStep.find(@options.step_3) unless @options.step_3.blank?
     
     render_paragraph :feature => :next_steps_page_view
   end
