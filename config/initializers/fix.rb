2b6b124ae64ee20e7fa3513835bc7f8ed1d069c3
Cykodcore <cykodcore@cykod.com>
2013-01-09 08:06:00 -0500

    Rails vulnerability fix

--- config/initializers/fix.rb
+++ config/initializers/fix.rb
@@ -1,2 +1,2 @@
 ActionController::Base.param_parsers.delete(Mime::XML) 
-// https://groups.google.com/forum/m/#!topic/rubyonrails-security/61bkgvnSGTQ/discussion
+# https://groups.google.com/forum/m/#!topic/rubyonrails-security/61bkgvnSGTQ/discussion
