5b23956a65b4382bb084a10f1e76225aaa85dd95
Pascal Rettig <pascal@cykod.com>
2013-05-29 17:22:36 -0400

    Remove filter lower case

--- app/models/user_segment/field_type.rb
+++ app/models/user_segment/field_type.rb
@@ -86,7 +86,6 @@ class UserSegment::FieldType
       end
     when :option
       unless value.is_a?(Array)
-        value = value.downcase if value.is_a?(String)
         value = opts[:options].find do |o|
           if o.is_a?(Array)
             o[1] == value
